47dd32f3a16a716cac74ab0ae84c3bc08bed4a02
Mai <maiabozeid2@gmail.com>
2022-07-31 02:53:56 +0200

    added title

---
+++ android/app/src/main/kotlin/com/example/islamiproject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.islamiproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes:{ HomeScreen.routeName:(_)=> HomeScreen(), },
+      initialRoute:HomeScreen.routeName,
+
+
+    );
+    }
+}
